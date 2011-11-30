195d8085b128b1aa121b2bb545bf86ea3a61f7f0
Ferdinando Villa <ferdinando.villa@gmail.com>
2011-11-30 16:11:48 +0100

    fixes

--- src/org/integratedmodelling/thinklab/api/project/IProject.java
+++ src/org/integratedmodelling/thinklab/api/project/IProject.java
@@ -12,25 +12,37 @@ import org.integratedmodelling.thinklab.api.modelling.INamespace;
  */
 public interface IProject {
 	
-	// properties for THINKLAB-INF/properties file
+	// properties for META-INF/thinklab.properties file
 	public static final String SOURCE_FOLDER_PROPERTY = "thinklab.source.folder";
+	public static final String ONTOLOGY_NAMESPACE_PREFIX_PROPERTY = "thinklab.ontology.prefix";
 	
 	public String getId();
 
 	public Properties getProperties();
-	
-	/**
-	 * Source folder is the root of the Thinklab package tree.
-	 * 
-	 * @return
-	 */
-	public File getSourceFolder();
-	
+		
 	/**
 	 * Return all the namespaces defined in the project.
 	 * 
 	 * @return
 	 */
 	public Collection<INamespace> getNamespaces();
+	
+	/**
+	 * Source folders are scanned at startup and monitored during development. The location of
+	 * source folders is in the SOURCE_FOLDER_PROPERTY of the thinklab properties.
+	 * @return
+	 */
+	public Collection<File> getSourceFolders();
+	
+	/**
+	 * Any ontologies in the source folder must have a URL that matches the project's 
+	 * ontology namespace prefix (from properties, defaulting to http://www.integratedmodelling.org/ns)
+	 * followed by the path corresponding to the ontology namespace and file name.
+	 * 
+	 * @return
+	 */
+	public String getOntologyNamespacePrefix();
+	
+	
 
 }
