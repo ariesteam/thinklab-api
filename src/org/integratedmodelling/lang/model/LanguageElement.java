aacf18249e28d856733c66d0240f928bc0cb89a1
Ferdinando Villa <ferdinando.villa@gmail.com>
2011-12-19 08:46:01 +0100

    API completion

--- src/org/integratedmodelling/lang/model/LanguageElement.java
+++ src/org/integratedmodelling/lang/model/LanguageElement.java
@@ -3,17 +3,25 @@ package org.integratedmodelling.lang.model;
 public class LanguageElement {
 
 	String id;
-	int    lineNumber;
+	int    lastLineNumber;
+	int    firstLineNumber;
+	
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
-	public int getLineNumber() {
-		return lineNumber;
+	public int getLastLineNumber() {
+		return lastLineNumber;
 	}
-	public void setLineNumber(int lineNumber) {
-		this.lineNumber = lineNumber;
+	public void setLastLineNumber(int lineNumber) {
+		this.lastLineNumber = lineNumber;
+	}
+	public int getFirstLineNumber() {
+		return firstLineNumber;
+	}
+	public void setFirstLineNumber(int firstLineNumber) {
+		this.firstLineNumber = firstLineNumber;
 	}
 }
