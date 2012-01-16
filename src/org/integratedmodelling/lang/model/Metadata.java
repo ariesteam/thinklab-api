ebc37dae2eaa4b5050b523292427c3754503220d
Ferdinando Villa <ferdinando.villa@gmail.com>
2012-01-16 15:20:15 +0100

    tiny improvs

--- src/org/integratedmodelling/lang/model/Metadata.java
+++ src/org/integratedmodelling/lang/model/Metadata.java
@@ -1,5 +1,13 @@
 package org.integratedmodelling.lang.model;
 
+import java.util.HashMap;
+
 public class Metadata extends ModelObject {
+
+	HashMap<String, Object> data = new HashMap<String, Object>();
+	
+	public void put(String id, Object value) {
+		data.put(id,value);
+	}
 
 }
--- src/org/integratedmodelling/lang/model/ModelObject.java
+++ src/org/integratedmodelling/lang/model/ModelObject.java
@@ -2,4 +2,16 @@ package org.integratedmodelling.lang.model;
 
 public class ModelObject extends LanguageElement {
 	
+	Metadata metadata;
+
+	public Metadata getMetadata() {
+		return metadata;
+	}
+
+	public void setMetadata(Metadata metadata) {
+		this.metadata = metadata;
+	}
+	
+	
+	
 }
