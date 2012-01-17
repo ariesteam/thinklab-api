ea875021eb2f18c69a92d59e8875d2325a6f29db
Ferdinando Villa <ferdinando.villa@gmail.com>
2012-01-17 17:26:54 +0100

    new model objects

--- src/org/integratedmodelling/lang/model/Model.java
+++ src/org/integratedmodelling/lang/model/Model.java
@@ -2,4 +2,15 @@ package org.integratedmodelling.lang.model;
 
 public class Model extends ModelObject {
 
+	/**
+	 * Add one observer with an optional conditional expression to contextualize the model to use.
+	 * 
+	 * @param observer
+	 * @param expression
+	 */
+	public void addConditional(Observer observer, Expression expression) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
