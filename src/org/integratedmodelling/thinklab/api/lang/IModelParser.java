7ddb36c9fb16449a75f0c92695c228e1b76133ce
Ferdinando Villa <ferdinando.villa@gmail.com>
2012-01-23 16:25:48 +0100

    separating compilation context from parser.

--- src/org/integratedmodelling/thinklab/api/lang/IModelParser.java
+++ src/org/integratedmodelling/thinklab/api/lang/IModelParser.java
@@ -23,19 +23,7 @@ public interface IModelParser {
 	 * @return
 	 * @throws ThinklabException
 	 */
-	public abstract Namespace parse(InputStream input) throws ThinklabException;
+	public abstract Namespace parse(InputStream input, ICompilationContext context) throws ThinklabException;
 	
-	/**
-	 * The parser should remember all the namespaces that are parsed and be able to resolve them after parsing
-	 * (for example as the result of an import statement). Also, if the namespace has been seen before it should
-	 * try to load it from any obvious location before failing. Loading may be based on the physical location of a
-	 * current repository and the namespace path, or on any externally provided reference (such as a URL) that 
-	 * is passed as a second parameter.
-	 *
-	 * @param namespace 
-	 * @param reference where to look for the namespace to load if not known. May be null.
-	 * @return
-	 * @throws ThinklabException
-	 */
-	public abstract Namespace resolveNamespace(String namespace, String reference) throws ThinklabException;
+
 }
