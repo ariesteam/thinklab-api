71028b8b2e2b68715ebedfe5418af4d51feb035f
Ferdinando Villa <ferdinando.villa@gmail.com>
2012-01-26 16:14:29 +0100

    Straightened out the namespace -> input stream resolution logics

--- src/org/integratedmodelling/thinklab/api/lang/IModelParser.java
+++ src/org/integratedmodelling/thinklab/api/lang/IModelParser.java
@@ -15,15 +15,30 @@ import org.integratedmodelling.lang.model.Namespace;
 public interface IModelParser {
 	
 	/**
-	 * Main entry point into the parser. It should normally not be called as the resolveNamespace() below is capable
-	 * of creating a namespace from an ID, but it can be used for code passed as a string or for operation outside of
-	 * a module.
+	 * Parse an input stream in a given namespace. Must not specify a namespace programmatically.
+	 * The result is a clone of the input namespace, but contains only those model objects and 
+	 * axioms that were declared in the input stream - i.e. a delta compared to the existing
+	 * namespace (or all of it if it didn't exist originally).
 	 * 
-	 * @param input
-	 * @return
+	 * @param input source language to be parsed into model objects to add to the namespace. It should not
+	 *        be legal to declare a namespace in it.
+	 * @param namespace a namespace ID that may identify a known namespace or not.
+	 * @return a namespace containing whatever NEW model objects and axioms have been parsed from the input.
+	 */
+	public abstract Namespace parseInNamespace(InputStream input, String namespace) throws ThinklabException;
+	
+
+	/**
+	 * Main entry point into the parser. Turns a resource into a Namespace bean, from which point on it's the job
+	 * of the implementation to use it. A IResolver is used to connect resource names to input streams, find namespace
+	 * sources across dependency chains, and handling errors and messages fed to it by the parser.
+	 * 
+	 * @param resource should identify a resolvable URI that the passed resolver can handle (second parameter in resolve).
+	 * @param context a resolver to connect uris and namespace ids to input streams 
+	 * @return a finished namespace bean to be turned into usable model objects by the implementation.
 	 * @throws ThinklabException
 	 */
-	public abstract Namespace parse(InputStream input, ICompilationContext context) throws ThinklabException;
+	public abstract Namespace parse(String resource, IResolver context) throws ThinklabException;
 	
 
 }
---
+++ src/org/integratedmodelling/thinklab/api/lang/IResolver.java
@@ -0,0 +1,77 @@
+package org.integratedmodelling.thinklab.api.lang;
+
+import java.io.InputStream;
+
+import org.integratedmodelling.exceptions.ThinklabException;
+import org.integratedmodelling.lang.model.Namespace;
+
+public interface IResolver {
+
+	
+	/**
+	 * Return the "default" namespace to be used when code is parsed from user input or other
+	 * non-namespace setting sources. This may be something simple like "user" or be linked to
+	 * a session ID or whatever else.
+	 * 
+	 * @return
+	 */
+	public abstract String getDefaultNamespaceId();
+	
+	/**
+	 * Override to fine-tune error management. If this throws an exception, the parser also will. If not, 
+	 * the return value determines whether parsing continue (true) or not (false).
+	 * 
+	 * @param e
+	 * @return
+	 */
+	public abstract boolean onException(Throwable e, int lineNumber) throws ThinklabException;
+
+	/**
+	 * Override to fine-tune error management. Will stop parsing if false is returned.
+	 * 
+	 * @param warning
+	 * @return
+	 */
+	public abstract boolean onWarning(String warning, int lineNumber);
+
+	/**
+	 * Override to fine-tune error management. Will stop parsing if false is returned.
+	 * 
+	 * @param info
+	 * @return
+	 */
+	public abstract boolean onInfo(String info, int lineNumber);
+
+	/**
+	 * Resolve a namespace (optionally further specified by an import URI) to an input stream. Either the namespace id or
+	 * the reference may be null, i.e. it must be prepared to produce an inputstream based on a namespace id only (e.g. 
+	 * resolving across dependent plugin classpaths) and/or based on an external reference URI; if both are present, 
+	 * it should ensure that they're compatible name-wise. 
+	 *
+	 * @param namespace. May be null if only the reference is passed.
+	 * @param reference where to look for the namespace to load if not known. May be null.
+	 * @return a valid input stream which will be read and closed by the parser.
+	 * @throws ThinklabException
+	 */
+	public abstract InputStream resolveNamespace(String namespace, String reference) throws ThinklabException;
+	
+	/**
+	 * Callback invoked as soon as a namespace declaration is parsed. Passed back the parameters that were
+	 * used in namespace resolution (see resolveNamespace for docs) both of which may be null. Should ensure that 
+	 * the namespace declaration is appropriate for the resources. Any imports will be parsed before this is called.
+	 * 
+	 * @param namespaceId
+	 * @param resourceId
+	 * @param namespace
+	 */
+	public abstract void onNamespaceDeclared(String namespaceId, String resourceId, Namespace namespace);
+	
+	/**
+	 * Callback invoked as soon as parsing of a namespace has been completed. The namespace will contain all
+	 * model objects declared in it and the axioms collected from them. 
+	 * 
+	 * @param namespace
+	 */
+	public abstract void onNamespaceDefined(Namespace namespace);
+	
+}
