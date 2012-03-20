72a69940ee84d74f94585591f09f6782c48d4eba
Ferdinando Villa <ferdinando.villa@gmail.com>
2012-03-20 08:07:19 +0100

    more refactoring, definition interfaces for model objects started

--- src/org/integratedmodelling/thinklab/api/lang/parsing/IModelObjectDefinition.java
+++ src/org/integratedmodelling/thinklab/api/lang/parsing/IModelObjectDefinition.java
@@ -2,4 +2,8 @@ package org.integratedmodelling.thinklab.api.lang.parsing;
 
 public interface IModelObjectDefinition extends ILanguageDefinition {
 
+	public void setNamespace(INamespaceDefinition namespace);
+	
+	public void setLineNumbers(int startLine, int endLine);
+	
 }
--- src/org/integratedmodelling/thinklab/api/lang/parsing/INamespaceDefinition.java
+++ src/org/integratedmodelling/thinklab/api/lang/parsing/INamespaceDefinition.java
@@ -1,5 +1,18 @@
 package org.integratedmodelling.thinklab.api.lang.parsing;
 
+import org.integratedmodelling.thinklab.api.knowledge.IAxiom;
+
 public interface INamespaceDefinition extends ILanguageDefinition {
 
+	public void setId(String id);
+	
+	public void addAxiom(IAxiom axiom);
+	
+	public void setResourceUrl(String resourceUrl);
+	
+	public void setTimeStamp(long timestamp);
+	
+	public void addImportedNamespace(INamespaceDefinition namespace);
+	
+	public void addModelObject(IModelObjectDefinition modelObject);
 }
