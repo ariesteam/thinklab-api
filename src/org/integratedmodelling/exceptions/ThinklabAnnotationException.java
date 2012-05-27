51be836e6b09b008d10b2f6afe2fa6783d9b5093
Ferdinando Villa <ferdinando.villa@gmail.com>
2012-05-27 14:14:09 +0200

    fixes, exceptions

---
+++ src/org/integratedmodelling/exceptions/ThinklabAnnotationException.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package org.integratedmodelling.exceptions;
+
+
+/**
+ * @author Ferd
+ *
+ */
+public class ThinklabAnnotationException extends ThinklabException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 752007760649260689L;
+
+	/**
+	 * 
+	 */
+	public ThinklabAnnotationException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public ThinklabAnnotationException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public ThinklabAnnotationException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public ThinklabAnnotationException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/org/integratedmodelling/thinklab/api/modelling/parsing/IFunctionDefinition.java
+++ src/org/integratedmodelling/thinklab/api/modelling/parsing/IFunctionDefinition.java
@@ -2,6 +2,8 @@ package org.integratedmodelling.thinklab.api.modelling.parsing;
 
 import java.util.Map;
 
+import org.integratedmodelling.thinklab.api.annotations.Concept;
+
 public interface IFunctionDefinition extends ILanguageDefinition {
 
 	public void set(String id, Map<String,Object> parameters);
